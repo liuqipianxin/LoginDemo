cc6d90560eff25c3c6352e351b283d11b0768be6
liuqipianxin <840323410@qq.com>
2017-12-20 13:12:46 +0800

    xiugai

--- src/main/java/com/zhiligeo/Login/LoginApplication.java
+++ src/main/java/com/zhiligeo/Login/LoginApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginApplication {
 	public static void main(String[] args) {
+		System.out.println("dad");
 		SpringApplication.run(LoginApplication.class, args);
 	}
 }
